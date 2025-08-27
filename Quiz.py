04d5c6ec2d37ed507b8c66dc6a31885e5c7910a1
BredeFj <bredefjellanger@gmail.com>
2025-08-27 12:20:27 +0200

    commets

--- Quiz.py
+++ Quiz.py
@@ -1,8 +1,16 @@
+# Skriptet starter et quiz-spill i terminalen
 print("Velkommen til dette Quiz-spillet!")
+
+# Spør brukeren om navn
 navn = input("Hva heter du? ")
+
+# Hilser brukeren velkommen
 print("Så kjekt at du vil være med " + navn + "!")
+
+# Setter poengsummen til 0
 poeng = 0
 
+# Første spørsmål og sjekker svaret
 svar1 = input("Første spørsmål: Hva er hovedstaden i Norge? ")
 if svar1.lower() == "oslo":
     print("Det er riktig! Godt jobbet :)")
@@ -10,6 +18,7 @@ if svar1.lower() == "oslo":
 else:
     print("Beklager, men det er feil svar.")
 
+# Andre spørsmål og sjekker svaret
 svar2 = input("Hva heter Norges nest største by? ")
 if svar2.lower() =="bergen":
     print("Det er riktig! Godt jobbet :)")
@@ -17,6 +26,7 @@ if svar2.lower() =="bergen":
 else:
     print("Beklager, men det er feil svar.")
 
+# Tredje spørsmål og sjekker svaret
 svar3 = input("Hva heter det beste fotballaget i Bergen? ")
 if svar3.lower() == "brann":
     print("Det er riktig! Godt jobbet :)")
@@ -24,6 +34,7 @@ if svar3.lower() == "brann":
 else:
     print("Beklager, men det er feil svar.")
 
+# Oppsummerer poengsummen og gir tilbakemelding
 print(f"Gratulerer {navn}! Du fikk {poeng} poeng!")
 if poeng == 3:
     print("Godt jobbet! Du fikk full pott :)")
